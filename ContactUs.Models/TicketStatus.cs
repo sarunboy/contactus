8df9d3a89c96e1e2031ac2692af6a87a1a34f053
Sarun <sarunla@aerothai.co.th>
2015-09-16 16:32:44 +0700

    Add Ticket and TicketStatus

---
+++ ContactUs.Models/TicketStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContactUs.Models
+{
+    public enum TicketStatus
+    {
+        New, Accecpted, Closed, Rejected
+    }
+}
